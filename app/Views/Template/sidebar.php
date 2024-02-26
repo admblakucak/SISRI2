b1dc6688ea33884203fb63cb33e0135fae312ac3
Alpinsil <alvinms1001@gmail.com>
2024-02-26 11:06:11 +0700

    feat: menambah fitur, sidebar skripsi hanya muncul jika revisi telah di acc

--- app/Views/Template/sidebar.php
+++ app/Views/Template/sidebar.php
@@ -69,29 +69,36 @@
 									<?php } ?>
 								</ul>
 							</li>
-							<li class=" slide">
-								<a class="side-menu__item" data-bs-toggle="slide" href="#"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
-										<path fill="currentColor" d="M4 4V22H20V24H4C2.9 24 2 23.1 2 22V4H4M15 7H20.5L15 1.5V7M8 0H16L22 6V18C22 19.11 21.11 20 20 20H8C6.89 20 6 19.1 6 18V2C6 .89 6.89 0 8 0M17 16V14H8V16H17M20 12V10H8V12H20Z" />
-									</svg> &nbsp; &nbsp;<span class="side-menu__label">Skripsi</span><i class="angle fe fe-chevron-down"></i></a>
-								<ul class="slide-menu">
-									<li><a class="slide-item" href="<?= base_url() ?>bimbingan_skripsi/<?= $pem1[0]->nip ?>">Bimbingan Skripsi</a></li>
-									<li><a class=" slide-item" href="<?= base_url() ?>daftar_sidang">Daftar Sidang</a></li>
-									<li><a class="slide-item" href="<?= base_url() ?>berkas_mhs_skripsi">Berkas</a></li>
-									<?php
-									if ($penguji1 != NULL) {
-									?>
-										<li><a class="slide-item" href="<?= base_url() ?>bimbingan_revisi_skripsi/<?= $penguji1[0]->nip ?>">Revisi Skripsi</a></li>
-									<?php
-									} elseif ($penguji2 != NULL) {
-									?>
-										<li><a class="slide-item" href="<?= base_url() ?>bimbingan_revisi_skripsi/<?= $penguji2[0]->nip ?>">Revisi Skripsi</a></li>
-									<?php
-									} elseif ($penguji3 != NULL) {
-									?>
-										<li><a class="slide-item" href="<?= base_url() ?>bimbingan_revisi_skripsi/<?= $penguji3[0]->nip ?>">Revisi Skripsi</a></li>
-									<?php } ?>
-								</ul>
-							</li>
+							<?php
+							$total_acc_dosen_penguji = $db->query("SELECT * FROM `tb_acc_revisi` WHERE `nim` ='" . session()->get('ses_id') . "'")->getResult();
+							// dd(count($total_acc_dosen_penguji));
+
+							if (count($total_acc_dosen_penguji) == 3) {
+							?>
+								<li class=" slide">
+									<a class="side-menu__item" data-bs-toggle="slide" href="#"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
+											<path fill="currentColor" d="M4 4V22H20V24H4C2.9 24 2 23.1 2 22V4H4M15 7H20.5L15 1.5V7M8 0H16L22 6V18C22 19.11 21.11 20 20 20H8C6.89 20 6 19.1 6 18V2C6 .89 6.89 0 8 0M17 16V14H8V16H17M20 12V10H8V12H20Z" />
+										</svg> &nbsp; &nbsp;<span class="side-menu__label">Skripsi</span><i class="angle fe fe-chevron-down"></i></a>
+									<ul class="slide-menu">
+										<li><a class="slide-item" href="<?= base_url() ?>bimbingan_skripsi/<?= $pem1[0]->nip ?>">Bimbingan Skripsi</a></li>
+										<li><a class=" slide-item" href="<?= base_url() ?>daftar_sidang">Daftar Sidang</a></li>
+										<li><a class="slide-item" href="<?= base_url() ?>berkas_mhs_skripsi">Berkas</a></li>
+										<?php
+										if ($penguji1 != NULL) {
+										?>
+											<li><a class="slide-item" href="<?= base_url() ?>bimbingan_revisi_skripsi/<?= $penguji1[0]->nip ?>">Revisi Skripsi</a></li>
+										<?php
+										} elseif ($penguji2 != NULL) {
+										?>
+											<li><a class="slide-item" href="<?= base_url() ?>bimbingan_revisi_skripsi/<?= $penguji2[0]->nip ?>">Revisi Skripsi</a></li>
+										<?php
+										} elseif ($penguji3 != NULL) {
+										?>
+											<li><a class="slide-item" href="<?= base_url() ?>bimbingan_revisi_skripsi/<?= $penguji3[0]->nip ?>">Revisi Skripsi</a></li>
+										<?php } ?>
+									</ul>
+								</li>
+							<?php } ?>
 						<?php
 						}
 						?>
